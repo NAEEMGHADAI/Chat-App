d919ca278d1a56722e41868890b54a7fef1d19bc
NAEEMGHADAI <naeemghadai007.com@gmail.com>
2022-01-05 17:56:45 +0530

    Message Ui

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -3,11 +3,68 @@ import MyMessage from "./MyMessage";
 import TheirMessage from "./TheirMessage";
 
 const ChatFeed = (props) => {
-	console.log(props);
-	const { chats, activeChat, userName, message } = props;
+	// console.log("props: ", props);
+	const { chats, activeChat, userName, messages } = props;
 	const chat = chats && chats[activeChat];
-	console.log(chat, userName, message);
-	return <div>ChatFeed</div>;
+
+	const renderMesseges = () => {
+		const keys = Object.keys(messages);
+		return keys.map((key, index) => {
+			const message = messages[key];
+
+			// if (message.userName === userName) {
+			// 	return <MyMessage key={index} message={message} />;
+			// } else {
+			// 	return <TheirMessage key={index} message={message} />;
+			// }
+
+			const lastMessageKey = index === 0 ? null : messages[keys[index - 1]];
+			const isMyMessage = message.sender.username === userName;
+			return (
+				<div key={`msg_${index}`} style={{ width: "100%" }}>
+					<div className="message-block">
+						{isMyMessage ? (
+							<MyMessage message={message} />
+						) : (
+							<TheirMessage
+								message={message}
+								lastMessage={messages[lastMessageKey]}
+							/>
+						)}
+					</div>
+					<div
+						className="read-receipts"
+						style={{
+							marginRight: isMyMessage ? "18px" : "0px",
+							marginLeft: isMyMessage ? "0px" : "68px",
+						}}
+					>
+						read-receipts
+					</div>
+				</div>
+			);
+		});
+	};
+
+	if (!chat) {
+		return <div>Loading...</div>;
+	}
+	// console.log("chat: ", chat, userName, messages);
+	return (
+		<div className="chat-feed">
+			<div className="chat-title-container">
+				<div className="chat-title">{chat?.title}</div>
+				<div className="chat-subtitle">
+					{chat.people.map((person) => `${person.person.username}`)}
+				</div>
+			</div>
+			{renderMesseges()}
+			<div style={{ height: "100px" }} />
+			<div className="message-form-container">
+				<MessageForm {...props} chatId={activeChat} />
+			</div>
+		</div>
+	);
 };
 
 export default ChatFeed;
--- src/components/MyMessage.jsx
+++ src/components/MyMessage.jsx
@@ -1,4 +1,27 @@
-const MyMessage = (props) => {
-	return <div>MyMessage</div>;
+const MyMessage = ({ message }) => {
+	if (message?.attachment?.length > 0) {
+		return (
+			<img
+				src={message.attachment[0].file}
+				alt="message-attachment"
+				className="message-image"
+				style={{ float: "right" }}
+			/>
+		);
+	}
+	const result = message.text.replace("<p>", " ").replace("</p>", " ");
+	return (
+		<div
+			className="message"
+			style={{
+				float: "right",
+				marginRight: "18px",
+				color: "white",
+				backgroundColor: "#3B2A50",
+			}}
+		>
+			{result}
+		</div>
+	);
 };
 export default MyMessage;
