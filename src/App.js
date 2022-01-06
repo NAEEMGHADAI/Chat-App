698f16236db4c60d195ade035ad46bc2a6c593a5
NAEEMGHADAI <naeemghadai007.com@gmail.com>
2022-01-06 18:59:33 +0530

    Ready to use

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import { ChatEngine, ChatList } from "react-chat-engine";
+import { ChatEngine } from "react-chat-engine";
 import ChatFeed from "./components/ChatFeed";
-// import IsTyping from "./components/IsTyping";
 import "./App.css";
 import LoginForm from "./components/LoginForm";
 
@@ -17,11 +16,6 @@ const App = () => {
 			renderChatFeed={(chatAppState) => {
 				return <ChatFeed {...chatAppState} />;
 			}}
-			renderChatList={(chatAppState) => <ChatList {...chatAppState} />}
-			// renderIsTyping={(typers) => {
-			// 	console.log(typers);
-			// 	return <IsTyping {...typers} />;
-			// }}
 		/>
 	);
 };
